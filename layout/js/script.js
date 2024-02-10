2517d9604213a6499067a120fd3241ba39883ec9
Lucas dos Santos Camilo <78448559+Lucas-Santos-Camilo@users.noreply.github.com>
2024-02-09 21:37:13 -0300

    Add files via upload

---
+++ layout/js/script.js
@@ -0,0 +1,63 @@
+$(document).ready(function(){  
+    $('.variacoes .opcoes .cores').on('click', function() {
+        $('.variacoes .opcoes .cores').removeClass('active');
+        $(this).addClass('active');
+    });  
+
+    $('#overlay').addClass('active');
+    $('#popup').addClass('active');
+
+    $('#closePopup, #overlay').click(function() {
+        $('#overlay').removeClass('active');
+        $('#popup').removeClass('active');
+    });
+
+    if ($(window).width() < 768) {
+        $('.footerTop .col-3').off('click').on('click', function(){
+            $('.footerTop .col-3').not(this).removeClass('active').find('ul').hide();            
+            $(this).toggleClass('active').find('ul').toggle();
+        });
+        $('.buscar img').click(function(){
+            $('#buscaMobile').toggleClass('d-none');
+        });
+    }
+    $('.listaProdutos').slick({
+        slidesToShow: 5,
+        slidesToScroll: 1,
+        dots: false,
+        arrows: true,
+        centerMode: true,
+        focusOnSelect: true,
+        responsive: [
+            {
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                    autoplay: true,
+                    arrows: false,
+                }
+            }
+        ]
+    });
+    if ($(window).width() < 768) {
+        $('#servicos .row').slick({
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            dots: false,
+            arrows: false,
+            centerMode: true,
+            focusOnSelect: true,
+            autoplay: true,
+        }); 
+        $('#parceiras .row').slick({
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            dots: false,
+            arrows: false,
+            centerMode: true,
+            focusOnSelect: true,
+            autoplay: true,
+        });
+    }
+});
